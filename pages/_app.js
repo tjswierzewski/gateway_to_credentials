3de8b04d17cdc4e1a6db8c202c7b5d761a79969a
Tim Swierzewski <TJswierzewski@gmail.com>
2021-11-22 20:33:45 -0500

    fix or operator

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import "../styles/globals.css";
 
-axios.defaults.baseURL = process.env.CMS_URL | "http://localhost:1337/";
+axios.defaults.baseURL = process.env.CMS_URL || "http://localhost:1337/";
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
